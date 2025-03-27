49d4e9f00fd9310830775461feff8fc0f65a8d1d
bhyunnie <dev.bhyunnie@gmail.com>
2025-03-27 20:16:09 +0900

    initial: 프로젝트 생성

---
+++ src/main/kotlin/bucket/assignment/bucketstore/BucketAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package bucket.assignment.bucketstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BucketAssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<BucketAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/bucket/assignment/bucketstore/BucketAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package bucket.assignment.bucketstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BucketAssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
